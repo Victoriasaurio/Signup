5fa0bebaee87f4211e63cfb921857bd2f9b252b4
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-12-01 19:49:13 -0600

    Password encryption and ID generation functions are used

--- controllers/create.js
+++ controllers/create.js
@@ -1,15 +1,13 @@
 const addUserOnDB = require('../db/methods/user')
+const encrypt = require('../utils/encrypt')
+const generateID = require('../utils/generateID')
 
 const create = async(req, res) => {
     try {
-        const body = req.body
-        let status = 0, message = {}
-        function validateUser(err, user) {
-            status = err ? 500:201
-            message = err ? {message: 'ERROR user not validate'}:{user}
-            res.status(status).send(message)
-        }
-        await addUserOnDB(body, validateUser)
+        req.body.password = encrypt(req.body.password, 10)
+        req.body._id = generateID()
+        await addUserOnDB(req.body)
+        res.status(201).send({ id: req.body._id, message: 'success'})
     }catch(err){
         res.status(500).send({message: err.message})
     }
